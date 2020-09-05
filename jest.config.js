ac2aa7e4f4b4d040873fa6f8fc63360aae245efa
Jose Carvalho <jcarvalho@iMac-de-Jose.local>
2020-09-05 14:14:28 -0300

    corrigido require do arquivo jestconfig

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 const { compilerOptions } = require('./tsconfig.json');
-const { pathsToModuleNameMapper } = require('ts-jest');
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
 
 comp
 
